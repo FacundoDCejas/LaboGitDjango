e39714505fe2ee7d5d3d5920643bc76064815e0e
FacundoDCejas <facundocejas1122@gmail.com>
2021-10-02 14:20:00 -0300

    Eliminado clave secreta

--- GitDjango/settings.py
+++ GitDjango/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ea_h!4s8=$##*^2@om3%q)*be+1^3()%+@q3l1$b!7mnu)-h+5'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
